5317098b5cb6cb665ebdc9939bacb0f4c1f458a2
Pablo Micolini <pablo.micolini@nutus.com.ar>
2015-03-28 12:48:09 -0300

    Mini animación del slide...

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,14 @@
 
 
 $(document).ready(function(){
-	$('.speakers').on('click', '.post', function(){
+	$('.speakers').on('click', '.post', function()
+	{
 		if($(this).find('.post-text').is(':visible')){
 			$(this).css('margin-bottom', '0px');
-			$(this).find('.post-text').hide();
+			$(this).find('.post-text').slideUp();
 		}else{
 			$(this).css('margin-bottom', '334px');
-			$(this).find('.post-text').show();
+			$(this).find('.post-text').slideDown();
 		}   
 	})
 })
