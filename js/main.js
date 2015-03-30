842d3994de10c953a4c21e823fac5156606a54ed
aschelstraete <aschelstraete@gmail.com>
2015-03-30 11:43:07 -0300

    Updated main.js

--- js/main.js
+++ js/main.js
@@ -22,20 +22,11 @@ headerNav();
 // Smooth scroll
 
 $("header a").on('click', function(e) {
-
-   // prevent default anchor click behavior
    e.preventDefault();
-
-   // store hash
    var hash = this.hash;
-
-   // animate
    $('html, body').animate({
        scrollTop: $(hash).offset().top
      }, 500, function(){
-
-       // when done, add hash to url
-       // (default click behaviour)
        window.location.hash = hash;
      });
 
