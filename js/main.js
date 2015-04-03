6ac6b12fb95a14d3a746597025df2bc9d2a2f185
Pablo Micolini <pablo.micolini@nutus.com.ar>
2015-04-03 02:31:17 -0300

    lógica para suscripción al newsletter

--- js/main.js
+++ js/main.js
@@ -52,6 +52,7 @@ $(document).ready(function() {
 	  
 	Grid.init();
 	
+	// tickets
 	$("#tickets .comprando").on('click', function(e) 
 	{
 		var id = $(this).attr("id");
@@ -73,6 +74,52 @@ $(document).ready(function() {
 		
 		return false;
 	});
+	
+	// newsletter
+	$("#newsletter button").on('click', function(e) 
+	{
+		$('#newsletter_fail').addClass('hide');
+		$('#newsletter_ok').addClass('hide');
+		$('#newsletter_repeat').addClass('hide');
+		
+		var mail_regexp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		var email = $("#newsletter input").val();
+		var extra_data = {EmailAddress:email, CustomField41:"Spanish"};//English, Portuguese
+
+		
+		var validar = true;
+		validar = validar && email.length > 0;
+		validar = validar && mail_regexp.test(email);
+		if(!validar) 
+		{
+			$('#newsletter_fail').removeClass('hide'); 				 	
+		} 
+		else 
+		{
+			$('#newsletter button').attr('disabled', 'disabled');
+			
+			$.ajax({
+				url: "http://nuevo.gentedeideas.com/subscribe.php",
+				type: "post",
+				data: {FormValue_Fields: extra_data, FormValue_ListID: "216", FormValue_Command: "Subscriber.Add", FormButton_Subscribe: "Subscribe"}
+			}).done(function(data) 
+			{
+				if(data.indexOf("Gracias")  > -1) 
+				{
+					$('#newsletter_ok').removeClass('hide');
+				} 
+				else 
+				{
+					$('#newsletter_repeat').removeClass('hide');
+					$('#newsletter button').attr('disabled', false);
+				}
+
+			});
+		}
+		return false;
+		event.preventDefault();
+	});
+
 });
 
 
