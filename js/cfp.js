a8d6ef819caa897d234bbe0fa66b88d8fa56883d
Pablo Micolini <pablo.micolini@nutus.com.ar>
2015-04-04 16:43:26 -0300

    cfp v0

---
+++ js/cfp.js
@@ -0,0 +1,57 @@
+
+$(document).ready(function() {
+	
+	$('.navbar').addClass('navbar-white');	
+	
+	// newsletter
+	$("#newsletter button").on('click', function(e) 
+	{
+		e.preventDefault();
+		
+		$('#newsletter_fail').addClass('hide');
+		$('#newsletter_ok').addClass('hide');
+		$('#newsletter_repeat').addClass('hide');
+		
+		var mail_regexp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		var email = $("#newsletter input").val();
+		var extra_data = {EmailAddress:email, CustomField41:"Spanish"};//English, Portuguese
+
+		
+		var validar = true;
+		validar = validar && email.length > 0;
+		validar = validar && mail_regexp.test(email);
+		if(!validar) 
+		{
+			$('#newsletter_fail').removeClass('hide'); 				 	
+		} 
+		else 
+		{
+			$('#newsletter button').attr('disabled', 'disabled');
+			
+			$.ajax({
+				url: "http://nuevo.gentedeideas.com/subscribe.php",
+				type: "post",
+				data: {FormValue_Fields: extra_data, FormValue_ListID: "216", FormValue_Command: "Subscriber.Add", FormButton_Subscribe: "Subscribe"}
+			}).done(function(data) 
+			{
+				if(data.indexOf("Gracias")  > -1) 
+				{
+					$('#newsletter_ok').removeClass('hide');
+				} 
+				else 
+				{
+					$('#newsletter_repeat').removeClass('hide');
+					$('#newsletter button').attr('disabled', false);
+				}
+
+			});
+		}
+		return false;
+		
+	});
+
+});
+
+
+
+
