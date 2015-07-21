d51ec87c0d20dbb96e17ded040912084373955af
aschelstraete <aschelstraete@gmail.com>
2015-07-21 18:33:13 -0300

    Migrated branch to original repository

--- js/cfp.js
+++ js/cfp.js
@@ -1,35 +1,20 @@
 
 $(document).ready(function() {
-	
-	$('.navbar').addClass('navbar-white');
-	
-	// fix para que el menu se cierre solo en mobile
-	$(document).on('click','.navbar-collapse.in',function(e) 
-	{
-		if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
-			$(this).collapse('hide');
-		}
-	});
-	
-	$("#menu-lang a").on('click',function(e) 
-	{
-		window.location = $(this).attr("href");
-	});
-	
+
 	// tipos de convocatoria
-	$("#cfp_details button").on('click', function(e) 
+	$("#cfp_details button").on('click', function(e)
 	{
 		var id = $(this).attr("id");
-		
+
 		if (!$(this).hasClass("selected"))
-		{			
+		{
 			$("#cfp_details button").removeClass("selected");
 			$(this).addClass("selected");
-			
+
 			$(".cfp_data").slideUp("fast");
-			
+
 			$("#" + id + "_data").slideDown("slow");
-			
+
 			$('html, body').stop().animate({
 				 'scrollTop': ($("#" + id + "_data").offset().top - 97)
 			}, 900, 'swing');
@@ -39,46 +24,46 @@ $(document).ready(function() {
 			$("#cfp_details button").removeClass("selected");
 			$(".cfp_data").slideUp("fast");
 		}
-		
+
 		return false;
 	});
-	
+
 	// newsletter
-	$("#newsletter button").on('click', function(e) 
+	$("#newsletter button").on('click', function(e)
 	{
 		e.preventDefault();
-		
+
 		$('#newsletter_fail').addClass('hide');
 		$('#newsletter_ok').addClass('hide');
 		$('#newsletter_repeat').addClass('hide');
-		
+
 		var mail_regexp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 		var email = $("#newsletter input").val();
 		var extra_data = {EmailAddress:email, CustomField41:$("#newsletter button").data("lang")};//Spanish, English, Portuguese
 
-		
+
 		var validar = true;
 		validar = validar && email.length > 0;
 		validar = validar && mail_regexp.test(email);
-		if(!validar) 
+		if(!validar)
 		{
-			$('#newsletter_fail').removeClass('hide'); 				 	
-		} 
-		else 
+			$('#newsletter_fail').removeClass('hide');
+		}
+		else
 		{
 			$('#newsletter button').attr('disabled', 'disabled');
-			
+
 			$.ajax({
 				url: "http://nuevo.gentedeideas.com/subscribe.php",
 				type: "post",
 				data: {FormValue_Fields: extra_data, FormValue_ListID: "216", FormValue_Command: "Subscriber.Add", FormButton_Subscribe: "Subscribe"}
-			}).done(function(data) 
+			}).done(function(data)
 			{
-				if(data.indexOf("Gracias")  > -1) 
+				if(data.indexOf("Gracias")  > -1)
 				{
 					$('#newsletter_ok').removeClass('hide');
-				} 
-				else 
+				}
+				else
 				{
 					$('#newsletter_repeat').removeClass('hide');
 					$('#newsletter button').attr('disabled', false);
@@ -87,7 +72,7 @@ $(document).ready(function() {
 			});
 		}
 		return false;
-		
+
 	});
 
 	// chevrones para propuestas
@@ -99,7 +84,3 @@ $(document).ready(function() {
 	  i.toggleClass('fa-chevron-up fa-chevron-down');
 	});
 });
-
-
-
-
--- js/main.js
+++ js/main.js
@@ -1,148 +1,112 @@
-//Background on header 
+//Background on header
 
 $(document).ready(function() {
-	
-	// fix para que el menu se cierre solo en mobile
-	$(document).on('click','.navbar-collapse.in',function(e) 
-	{
-		if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
-			$(this).collapse('hide');
+
+
+	var addBgrTop = $('#speakers').offset().top -1;
+
+	var headerNav = function() {
+		var scrollTop = $(window).scrollTop();
+
+		if (scrollTop > addBgrTop) {
+			$('.navbar').addClass('navbar-white');
+		} else {
+			$('.navbar').removeClass('navbar-white');
 		}
-	});
-	
-	$("#menu-lang a, #buscabasisa14, .go_link").on('click',function(e) 
-	{
-		window.location = $(this).attr("href");
-	});
-	
-	var addBgrTop = $('#speakers').offset().top -1;  
-		  
-	var headerNav = function()
-	{  
-		var scrollTop = $(window).scrollTop();  
-			   
-		if (scrollTop > addBgrTop) {   
-			$('.navbar').addClass('navbar-white');  
-		} else {  
-			$('.navbar').removeClass('navbar-white');   
-		}  
 	};
-	
+
 	// ocultamos los signitos de pregunta en modo celular
 	if (parseInt($(window).width()) < 768)
 	{
 		$('.speaker-soon:not(:first)').parent().remove();
 	}
-	
+
 	// el menu es autocoloreable solo en modo escritorio, en modo mobile lo mostramos siempre blanco
 	if (parseInt($(window).width()) > 1075)
 	{
-		 headerNav();  
-		
-		$(window).scroll(function() {  
-			headerNav();  
+		 headerNav();
+
+		$(window).scroll(function() {
+			headerNav();
 		});
 	}
 	else
 	{
 		$('.navbar').addClass('navbar-white');
 	}
-	
-	$("header a").on('click', function(e) 
-	{
-		// prevent default anchor click behavior
-		e.preventDefault();
-
-		// store hash
-		var hash = this.hash;
-
-		// animate
-		$('html, body').animate({
-		   scrollTop: $(hash).offset().top
-		}, 500, function(){
-
-		   // when done, add hash to url
-		   // (default click behaviour)
-		   window.location.hash = hash;
-		});
-	});
-	
-	var headroom  = new Headroom(document.querySelector("header"));
-	headroom.init();
-	  
 	$.stellar({
 		horizontalScrolling: false,
 		verticalOffset: 40
 	});
-	  
+
 	$('[data-toggle="tooltip"]').tooltip()
-	  
+
 	Grid.init();
-	
+
 	// tickets
-	$("#tickets .comprando").on('click', function(e) 
+	$("#tickets .comprando").on('click', function(e)
 	{
 		var id = $(this).attr("id");
-		
+
 		if (!$(this).hasClass("selected"))
-		{			
+		{
 			$("#tickets .comprando").removeClass("selected");
 			$(this).addClass("selected");
-			
+
 			$(".buy_form").slideUp("fast");
-			
+
 			$("#" + id + "_form").slideDown("slow");
-			
+
 			$('html, body').stop().animate({
 				 'scrollTop': ($("#" + id + "_data").offset().top - 97)
 			}, 900, 'swing');
-			
+
 		}
 		else
 		{
 			$("#tickets .comprando").removeClass("selected");
 			$(".buy_form").slideUp("fast");
 		}
-		
+
 		return false;
 	});
-	
-	// newsletter
-	$("#newsletter button").on('click', function(e) 
+
+// newsletter
+	$("#newsletter button").on('click', function(e)
 	{
 		e.preventDefault();
-		
+
 		$('#newsletter_fail').addClass('hide');
 		$('#newsletter_ok').addClass('hide');
 		$('#newsletter_repeat').addClass('hide');
-		
+
 		var mail_regexp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 		var email = $("#newsletter input").val();
 		var extra_data = {EmailAddress:email, CustomField41:$("#newsletter button").data("lang")};//Spanish, English, Portuguese
 
-		
+
 		var validar = true;
 		validar = validar && email.length > 0;
 		validar = validar && mail_regexp.test(email);
-		if(!validar) 
+		if(!validar)
 		{
-			$('#newsletter_fail').removeClass('hide'); 				 	
-		} 
-		else 
+			$('#newsletter_fail').removeClass('hide');
+		}
+		else
 		{
 			$('#newsletter button').attr('disabled', 'disabled');
-			
+
 			$.ajax({
 				url: "http://nuevo.gentedeideas.com/subscribe.php",
 				type: "post",
 				data: {FormValue_Fields: extra_data, FormValue_ListID: "216", FormValue_Command: "Subscriber.Add", FormButton_Subscribe: "Subscribe"}
-			}).done(function(data) 
+			}).done(function(data)
 			{
-				if(data.indexOf("Gracias")  > -1) 
+				if(data.indexOf("Gracias")  > -1)
 				{
 					$('#newsletter_ok').removeClass('hide');
-				} 
-				else 
+				}
+				else
 				{
 					$('#newsletter_repeat').removeClass('hide');
 					$('#newsletter button').attr('disabled', false);
@@ -151,11 +115,7 @@ $(document).ready(function() {
 			});
 		}
 		return false;
-		
+
 	});
 
 });
-
-
-
-
