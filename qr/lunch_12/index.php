76db4912830d55d92eaf8916a5577de625a7bfe2
Pablo Micolini <pablo.micolini@nutus.com.ar>
2015-11-10 21:47:12 -0300

    Mapas de comidas para QR

---
+++ qr/lunch_12/index.php
@@ -0,0 +1,6 @@
+<?php
+
+header("Location: http://isa.ixda.org/2015/qr/lunch_12/lunch_12.pdf");
+exit;
+
+?>
---
+++ qr/lunch_34/index.php
@@ -0,0 +1,6 @@
+<?php
+
+header("Location: http://isa.ixda.org/2015/qr/lunch_34/lunch_34.pdf");
+exit;
+
+?>
